67fe21080359a0cdf607bbb357ccfa3874b17a9a
Hyeon Young Jang <gusdud9124@gmail.com>
2025-03-05 21:15:33 +0900

    feat: InitConfig를 활용한 의존성 주입 및 애플리케이션 실행 구현
    - InitConfig를 사용하여 TaskController 객체를 생성
    - ToDoListController를 생성하고 run() 메서드 실행
    - 프로그램 실행을 위한 엔트리포인트 구성 완료

--- src/main/java/com/memoryh/todolist/Application.java
+++ src/main/java/com/memoryh/todolist/Application.java
@@ -1,9 +1,14 @@
 package com.memoryh.todolist;
 
+import com.memoryh.todolist.config.InitConfig;
+import com.memoryh.todolist.controller.ToDoListController;
+
 public class Application {
 
     public static void main(String[] args) {
-
+        InitConfig initConfig = new InitConfig();
+        ToDoListController toDoListController = new ToDoListController(initConfig.taskController());
+        toDoListController.run();
     }
 
 }
