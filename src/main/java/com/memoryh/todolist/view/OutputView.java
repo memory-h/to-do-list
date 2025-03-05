82b1aa92a508b07f06358872f834f12161b4208f
Hyeon Young Jang <gusdud9124@gmail.com>
2025-03-05 21:29:33 +0900

    feat: TO-DO LIST 프로그램 시작을 알리는 메시지를 출력하는 기능을 구현

---
+++ src/main/java/com/memoryh/todolist/view/OutputView.java
@@ -0,0 +1,11 @@
+package com.memoryh.todolist.view;
+
+public class OutputView {
+
+    private OutputView() {}
+
+    public static void printWelcomeMessage() {
+        System.out.println("TO-DO LIST 프로그램을 시작합니다.");
+    }
+
+}
