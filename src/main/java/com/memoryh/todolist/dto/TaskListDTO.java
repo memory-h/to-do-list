af47b994b5dbc75ec586206aa56b41cebaeab604
Hyeon Young Jang <gusdud9124@gmail.com>
2025-03-06 10:57:06 +0900

    refactor: private 접근 제한자 추가, 필드 레벨에서 List를 초기화

--- src/main/java/com/memoryh/todolist/dto/TaskListDTO.java
+++ src/main/java/com/memoryh/todolist/dto/TaskListDTO.java
@@ -1,18 +1,17 @@
 package com.memoryh.todolist.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class TaskListDTO {
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    List<TaskDTO> tasks;
+    private List<TaskDTO> taskDTOList = new ArrayList<>();
 
 }
