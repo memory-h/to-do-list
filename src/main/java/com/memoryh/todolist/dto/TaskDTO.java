4be7bac76adbd49225283aaa75c63408c2c3bf6c
Hyeon Young Jang <gusdud9124@gmail.com>
2025-03-06 22:34:10 +0900

    feat: 기본 생성자 추가

--- src/main/java/com/memoryh/todolist/dto/TaskDTO.java
+++ src/main/java/com/memoryh/todolist/dto/TaskDTO.java
@@ -3,11 +3,14 @@ package com.memoryh.todolist.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonPropertyOrder({"id", "title", "dueDate", "completed"})
 public class TaskDTO {
 
